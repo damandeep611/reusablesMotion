e86a5c6d9726c635f20cb31efb5d65935f7709c7
devDaman <damandeep611@gmail.com>
2025-03-09 19:46:52 +0530

    sidebar

--- src/app/(docs)/docsLayout/sidebar.constants.ts
+++ src/app/(docs)/docsLayout/sidebar.constants.ts
@@ -43,10 +43,98 @@ export const DOCS: Documentation[] = [
     groupKey: "components",
     groupValue: "Reusables Components",
     children: [
+      { label: "Alert", value: "alert", url: "/docs/alert", new: true },
       {
         label: "Animated Feature Section",
         value: "animatedfeaturesection",
         url: "/docs/gridfeature",
+      },
+      {
+        label: "Animated Feature ",
+        value: "animatedfeature",
+        url: "/docs/animatednew",
+      },
+      {
+        label: "Animated Testimonials",
+        value: "animatedtestimonials",
+        url: "/docs/animatedtestimonials",
+      },
+      {
+        label: "Autosize Textarea",
+        value: "autosize-textarea",
+        url: "/docs/autosize-textarea",
+      },
+      { label: "Button", value: "button", url: "/docs/button" },
+      { label: "Card", value: "card", url: "/docs/card" },
+      {
+        label: "Datetime Picker",
+        value: "datetime-picker",
+        url: "/docs/datetime-picker",
+      },
+      {
+        label: "Dual Range Slider",
+        value: "dual-range-slider",
+        url: "/docs/dual-range-slider",
+      },
+      {
+        label: "Event Calendar",
+        value: "eventcalendar",
+        url: "/docs/eventcalendar",
+      },
+      {
+        label: "Feadback Card",
+        value: "feadback",
+        url: "/docs/feadback",
+        new: true,
+      },
+      {
+        label: "Floating Label Input",
+        value: "floating-label-input",
+        url: "/docs/floating-label-input",
+      },
+      { label: "Footer", value: "footer", url: "/docs/footer" },
+      {
+        label: "HTTP Status Code",
+        value: "statuscode",
+        url: "/docs/statuscode",
+      },
+      {
+        label: "Image Preview",
+        value: "imagepreview",
+        url: "/docs/imagepreview",
+      },
+      {
+        label: "Infinite Scroll",
+        value: "infiniteScroll",
+        url: "/docs/infinite-scroll",
+      },
+      { label: "Input", value: "input", url: "/docs/input" },
+      {
+        label: "Loading Button",
+        value: "loading-button",
+        url: "/docs/loading-button",
+      },
+      {
+        label: "Multiple Selector",
+        value: "multipleSelector",
+        url: "/docs/multiple-selector",
+      },
+      { label: "Navbar", value: "navbar", url: "/docs/navbar", new: true },
+      {
+        label: "Progress With Value",
+        value: "progress-with-value",
+        url: "/docs/progress-with-value",
+      },
+      {
+        label: "Responsive Modal",
+        value: "responsive-modal",
+        url: "/docs/responsive-modal",
+      },
+      { label: "Spinner", value: "spinner", url: "/docs/spinner" },
+      {
+        label: "Skeleton",
+        value: "skeleton",
+        url: "/docs/skeleton",
       },
     ],
   },
